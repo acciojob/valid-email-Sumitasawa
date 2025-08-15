43aa47b2a841a73ab6a9c326e8d3ebcb3490260c
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-15 19:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
   //your JS code here.
+	 if (!str) return false;
+	 const regex = /^[\w.-]+@[\w.-]+\.\w{2,}$/;
+	if (str.includes("..") || str.includes("--")) return false;
+	return regex.test(str);
 }
 
 // Do not change the code below.
